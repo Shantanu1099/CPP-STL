a9eaac491c012553944e02f5d8b05460f9be418c
Shantanu Singh <38537606+Shantanu1099@users.noreply.github.com>
2021-09-10 21:23:17 +0530

    Add files via upload

---
+++ STL_Iterators.cpp
@@ -0,0 +1,145 @@
+#include<bits/stdc++.h>
+#include<typeinfo>
+using namespace std;
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define forinput for(int i =0;i<n;i++){cin>>arr[i];}
+#define foroutput for(int i = 0;i<n;i++){cout<<arr[i]<<" ";}
+#define pb push_back
+#define pob pop_back
+typedef string ss;
+
+
+int main(){
+	// vector<vector<pair<int, int>> > v;
+	// int n; cin >> n;
+	// vector<pair<int, int> > temp;
+	// for(int i = 0; i < n ; ++i){
+	// 	int x, y; cin >> x >> y;
+	// 	temp.pb(make_pair(x, y));
+	// }
+	// v.pb(temp);
+	// cout<<v[0].size();
+	// for(int i = 0 ; i < v[0].size(); ++i){
+	// 	cout << v[0][i].first << " " << v[0][i].second << endl;
+	// }
+
+	// vector<vector<pair<int, int>>> v = {{{33, 44}, {443, 665}}, {{34,43}, {5343, 35}}};
+	// FOr VECTOR OF VECTORS OF PAIRS
+	// vector<vector<pair<int, int>>> :: iterator row;
+	// vector<pair<int, int>> :: iterator col;
+	// for( row = v.begin(); row != v.end(); ++row){
+	// 	for(col = row->begin(); col != (*row).end(); ++col){
+	// 		cout << col->first << " " << col->second << endl;
+	// 	}
+	// 	cout << endl;
+	// }
+
+
+
+	// For VECTOR of VECTORS...
+	// vector<vector<int>> :: iterator row;
+	// vector<int> :: iterator col;
+	// for( row = v.begin(); row != v.end(); ++row){
+	// 	for(col = (*row).begin(); col != (*row).end(); ++col){
+	// 		cout << (*col) << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+
+
+	// vector<pair<int, int>> v = {{33, 44}, {443, 665}};
+	// vector<int> v1 = {33, 44, 443, 665};
+
+
+
+	// vector<vector<pair<int,int>>> v = {{{43, 353}, {544, 665}}, {{65, 88}, {66, 99}}};
+	// cout<<v[0][1].first << " " << v[0][1].second << endl;
+	// vector<pair<int, int>> :: iterator it ;
+	// for(int i = 0 ; i < v.size(); ++i){
+	// 	for(it = v[i].begin(); it != v[i].end(); ++it){
+	// 	cout << (it->first) << " " << (it->second) << endl;
+	// 	// cout << (*it).first << " " << (*it).second << endl;
+	// }	
+	// }
+	// for(it = v[0].begin(); it != v[0].end(); ++it){
+	// 	// cout << (it->first) << " " << (it->second) << endl;
+	// 	cout << (*it).first << " " << (*it).second << endl;
+	// }
+	// cout << endl;
+
+
+
+
+	// Short - for loops - (Range Based)
+	vector<int> v1 = {43, 353, 544, 665, 65};
+ 	for(int value: v1 ){
+		cout << value << endl;
+	}
+	cout<<endl;
+
+
+	// Example of Passig through reference and doing increment operation otherwise only the 'COPY' is Passed
+	for(int &value: v1 ){
+		value++;
+	}
+
+	for(int value: v1 ){
+		cout << value << endl;
+	}
+	cout<<endl;
+	// for(pair<int, int> value: v ){
+	// 	cout << value.first << " " << value.second << endl;
+	// }
+
+
+
+
+	// Short Loops - Auto Keyword & Range Based Combined for better readability and clean code
+	// Auto Keyword determines the datatype Automatically
+
+	auto a = 11.33;
+	cout << typeid(a).name() << " - " << a << endl;
+	for(auto &ip : v1){
+		cout << ip << " " ;
+	}
+	cout << endl;
+	for(auto &ip : v1){
+		ip++;
+		cout << ip << " " ;
+	}
+	cout << endl;
+	for(auto i = 0; i < v1.size(); ++i){
+		cout << v1[i] << " ";
+	}
+	cout << endl;
+	vector<pair<int, int>> v2 = {{43, 33}, {29, 224}};
+
+	for(auto ele : v2){
+		cout << ele.first << " " << ele.second << endl;
+	}
+
+
+	vector<vector<int> >  vv_arr = {{2, 3, 4, 5, 6}, {4, 3535, 33, 232}};
+	for(auto & ele : vv_arr){
+		for( auto & ele2 : ele){
+			cout << ele2 << " ";
+		}
+		cout << endl;
+	}
+	cout << endl << endl;
+
+	// vvi :: iterator vv_itr ;
+	// vi :: iterator v_itr;   No use b'coz we are using "auto" keyword
+	for(auto vv_itr = begin(vv_arr); vv_itr != end(vv_arr); ++vv_itr){
+		for(auto v_itr = vv_itr->begin(); v_itr != (*vv_itr).end(); ++v_itr){
+			cout << (*v_itr) << " ";
+		} 
+		cout << endl;
+	}
+
+	return 0;
+}
---
+++ STL_Maps.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define forinput for(int i =0;i<n;i++){cin>>arr[i];}
+#define foroutput for(int i = 0;i<n;i++){cout<<arr[i]<<" ";}
+#define pb push_back
+#define pob pop_back
+
+void iterMap(map<int, int> mm){
+	cout << "Size: " << mm.size() << endl; 
+	for(auto &k_v : mm){ // Accesing n element in a map and printing them orderly takes O(nlog(n))
+	cout << k_v.first << " " << k_v.second << endl;
+}
+}
+int main(){ 
+// While initialising strings as keys the T.C. increases to O(s.size()*log(n)) while insertion b'coz of the self/lexiographical sorting technique of Red-Black Trees in the keys part....That's why it is an ordered 'map'...
+
+// IN case of  'unordered_map <int, string> mmm;'  Everything is same except -> Inbuilt Implementation which is Hashmap.. and because Hashmap is used so Insertion and Accessement T.C. -> turns O(1) & keys datatypes -> Cannot use complex datatypes such as Containers must use int, float, long long, double these already defined fixed size Datatypes....
+
+
+// The major difference of 'multimap<int, string> mmmm;' when compared to maps is that it can store multiple similar value of key... The concept of Unique Key isn't available here..        You can do the same thing using map as well like.... map<int, vector<string> > mmod;
+
+// map<int , string > mm;
+// mm[1] = "LALALAL";
+// mm[2];
+// for(auto &k_v : mm){
+// 	cout << k_v.first << " " << k_v.second << endl;
+// }
+
+
+map< int, int> m;   // Can put any Container inside the < > thing... keys rae unique..i.e. There can'r be same key for multiple values...Cannot store more than 1 value...& that will be the latest Assigned value
+m[1] = 67;
+m[5] = 432;
+m[4] = 2323;
+m.insert({3, 222}); //inserting pair....
+m[3]; // Inserting a key as well takes a time of (O(log n)) & so does Accessing it.
+
+auto it = m.find(8);  // O(log(n))
+
+if( it != m.end()){   // Gives Segmentation Fault when Any value at or beyond m.end() is Accessed
+m.erase(it); // O(log(n))
+}
+
+if(it == m.end()) cout << "NOT FOUND" << endl;
+
+else  cout << "FOUND: " << it->first << " " << it->second << endl; 
+
+m.clear(); //clears all pairs in maps
+iterMap(m);
+
+
+
+
+// Below are three ( 3 ) ways of writing Iterating statement, choose what you like most
+
+// map<int, int> :: iterator it;
+// for( it = m.begin() ; it != m.end(); ++it){
+// 	cout << it->first << " " << it->second <<endl;
+// }
+
+// for(pair<int , int> pp: m){
+// 	cout << pp.first << " " << pp.second << endl;  
+// }
+
+// for(auto &pr: m){ // (O(nlogn)) coz we are accessing n elements n = 5 ;}
+
+// 	cout << pr.first << " " << pr.second << endl;  
+// }
+// cout << m[1] << endl << m[5] <<	endl << m[4] << endl;
+
+	return 0;
+}
